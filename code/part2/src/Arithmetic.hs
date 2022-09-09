122248dd563310ad9f738937d2bfa93065083e37
JP2511 <joaopedro.de.mendonca@gmail.com>
2022-09-09 16:12:10 +0200

    Correcting some bugs in evalErr

--- code/part2/src/Arithmetic.hs
+++ code/part2/src/Arithmetic.hs
@@ -77,22 +77,23 @@ extendEnv :: VName -> Integer -> Env -> Env
 extendEnv name value env = \x -> if x == name then Just value else env x
 
 
-applyFullEval :: Exp -> Exp -> (Integer -> Integer -> Integer) -> Integer
-applyFullEval x y f   = f (evalFull x) (evalFull y)
+applyFullEval :: Exp -> Exp -> (Integer -> Integer -> Integer) ->
+  Env -> Integer
+applyFullEval x y f env  = f (evalFull x env) (evalFull y env)
 
 
 evalFull :: Exp -> Env -> Integer
-evalFull (Cst x)   = x
-evalFull (Add x y) = applyFullEval x y (+)
-evalFull (Sub x y) = applyFullEval x y (-)
-evalFull (Mul x y) = applyFullEval x y (*)
-evalFull (Div x y) = if b == 0 || a < b
+evalFull (Cst x)   _   = x
+evalFull (Add x y) env = applyFullEval x y (+) env
+evalFull (Sub x y) env = applyFullEval x y (-) env
+evalFull (Mul x y) env = applyFullEval x y (*) env
+evalFull (Div x y) env = if b == 0 || a < b
   then error "Division where numerator is smaller than denominator"
   else div a b
   where
-    a = evalFull x
-    b = evalFull y
-evalFull (Pow x y) = applyFullEval x y (^)
+    a = evalFull x env
+    b = evalFull y env
+evalFull (Pow x y) env = applyFullEval x y (^) env
 evalFull (If test yes no)       env = if (evalFull test env) /= 0 
   then evalFull yes env
   else evalFull no  env
@@ -112,7 +113,7 @@ evalFull x _ = evalSimple x
 --  Problem 3
 
 parseArgs :: Exp -> Exp -> Env -> Either ArithError (Integer, Integer)
-applyOpOnExpErr x y f env = errorParser a b where
+parseArgs x y env = errorParser a b where
   a = evalErr x env
   b = evalErr y env
   errorParser (Left e)  _         = Left e
@@ -124,7 +125,7 @@ applyFuncErr :: Either ArithError (Integer, Integer) ->
                 (Integer -> Integer -> Integer) ->
                 Either ArithError Integer
 applyFuncErr (Left e)    _ = Left e
-applyFuncErr (Right a b) f = Right (f a b)
+applyFuncErr (Right (a, b)) f = Right (f a b)
 
 
 parser :: Exp -> Exp -> (Integer -> Integer -> Integer) -> Env ->
@@ -134,40 +135,44 @@ parser x y f env = applyFuncErr (parseArgs x y env) f
 
 evalErr :: Exp -> Env -> Either ArithError Integer
 -- Constant
-evalErr (Cst x) env = Right x
+evalErr (Cst x)   _   = Right x
 -- Addition
-evalErr (Add x y) = parser x y (+)
+evalErr (Add x y) env = parser x y (+) env
 -- Subtraction
-evalErr (Sub x y) = parser x y (-)
+evalErr (Sub x y) env = parser x y (-) env
 -- Multiplication
-evalErr (Mul x y) = parser x y (*)
+evalErr (Mul x y) env = parser x y (*) env
 -- Division
-evalErr (Div x y) env = f $ parseArgs x y env
+evalErr (Div x y) env = f $ parseArgs x y env where
   f (Right (a, b)) 
     | b == 0 = Left EDivZero
-    | a < b = Left (EOther "Division -> numerator smaller than denominator")
+    | a < b  = Left (EOther "Division -> numerator smaller than denominator")
     | otherwise = Right (div a b) 
-  f e = e
+  f (Left e) = Left e
+-- Power
+evalErr (Pow x y) env = f $ parseArgs x y env where
+  f (Right (a, b)) = if b < 0 then Left ENegPower else Right ((^) a b)
+  f (Left e)       = Left e
 -- Conditional
 evalErr (If test yes no) env = f $ evalErr test env where
   f (Right a) = if a /= 0 then evalErr yes env else evalErr no env
-  f e         = e
+  f (Left e)  = Left e
 -- Variable
 evalErr (Var v) env = f . env $ v where
   f Nothing  = Left (EBadVar v)
   f (Just x) = Right x
 -- Equation
-evalErr (Let var def body) env = f (evalErr def env)
-  f (Right val) = evalFull body $ extendEnv var value env
-  f e           = e
+evalErr (Let var def body) env = f (evalErr def env) where
+  f (Right val) = evalErr body $ extendEnv var val env
+  f (Left e)    = Left e
 -- Sum
-evalErr (Sum var from to body) env = f $ parseArgs from to env where
+evalErr (Sum var from to body) env = f (parseArgs from to env) where
   f (Right (a, b)) = foldr g (Right 0) (enumFromTo a b) where
     g _ (Left e)    = Left e
     g x (Right acc) = h (evalErr body $ extendEnv var x env) where
       h (Right z) = Right $ z + acc
       h e         = e
-  f e = e
+  f (Left e) = (Left e)
 
 
 -- ----------------------------------------------------------------------------
